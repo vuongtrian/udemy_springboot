b74be5a5cd4175d25c5fef35436fb2bc72b40b3f
An Vuong <anvuong@r2integrated.com>
2023-01-01 18:59:42 -0600

    update AOP @Around

---
+++ spring-demo-aop-around-with-logger/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,125 @@
+package com.luv2code.aopdemo.aspect;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	@Around("execution(* com.luv2code.aopdemo.service.*.getFortune(..))")
+	public Object aroundGetFortune(
+			ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
+
+		// print out which method we are advising on
+		String method = theProceedingJoinPoint.getSignature().toShortString();
+		myLogger.info("\n====>>> Executing @Around on method: " + method);
+		
+		// get begin timestamp
+		long begin = System.currentTimeMillis();
+		
+		// now, let's execute the method
+		Object result = theProceedingJoinPoint.proceed();
+		
+		// get end timestamp
+		long end = System.currentTimeMillis();
+		
+		// compute duration and display it
+		long duration = end - begin;
+		myLogger.info("\n====>>>> Duration: " + duration / 1000.0 + " seconds");
+		
+		return result;
+	}
+	
+	@After("execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))")
+	public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
+		
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		myLogger.info("\n====>>> Executing @After (finally) on method: " + method);
+	}
+	
+	@AfterThrowing(
+			pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+			throwing = "theExc")
+	public void afterThrowingFindAccountsAdvice(
+			JoinPoint theJoinPoint, Throwable theExc) {
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		myLogger.info("\n====>>> Executing @AfterThrowing on method: " + method);
+		
+		// log the exception
+		myLogger.info("\n====>>> The exception is: " + theExc);
+	}
+
+	@AfterReturning(
+			pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))", 
+			returning = "result")
+	public void afterReturningFindAccountsAdvice(
+			JoinPoint theJoinPoint, List<Account> result) {
+		
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		myLogger.info("\n====>>> Executing @AfterReturning on method: " + method);
+		
+		// print out the results of the method call
+		myLogger.info("\n====>>> Result is: " + result);
+		
+		// let's post-process the data ... let's modify it
+		// convert the account name to uppercase
+		convertAccountNameToUpperCase(result);
+	}
+	
+		
+	private void convertAccountNameToUpperCase(List<Account> result) {
+		for(Account tempAccount : result) {
+			String theUpperName = tempAccount.getName().toUpperCase();
+			tempAccount.setName(theUpperName);
+		}
+		
+	}
+
+
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinpoint) {
+		myLogger.info("\n====>>> Executing @Before advice on method");
+		
+		// display method signature
+		MethodSignature methodSig = (MethodSignature) theJoinpoint.getSignature();
+		myLogger.info("Method: " + methodSig);
+		
+		// display method arguments
+		
+		// get arguments
+		Object[] args = theJoinpoint.getArgs();
+		
+		// loop thru arguments
+		for (Object tempArg : args) {
+			myLogger.info(tempArg.toString());
+			
+			if (tempArg instanceof Account) {
+				Account theAccount = (Account) tempArg;
+				myLogger.info("Account name: " + theAccount.getName());
+				myLogger.info("Account level: " + theAccount.getLevel());
+			}
+		}
+	}
+	
+}
