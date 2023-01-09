6a651251edc4fc9c72b9e6031f4a25e265551430
An Vuong <anvuong@r2integrated.com>
2023-01-09 11:52:15 -0600

    finished Rest API real-time project

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/config/DemoAppConfig.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/config/DemoAppConfig.java
@@ -36,8 +36,6 @@ public class DemoAppConfig implements WebMvcConfigurer {
 	
 	private Logger logger = Logger.getLogger(getClass().getName());
 	
-	// define a bean for ViewResolver
-
 	@Bean
 	public DataSource myDataSource() {
 		
